3c2685fd198738dc50347cac16d3f0a31815fc85
metenar <metenar@gmail.com>
2021-04-26 16:02:00 -0700

    creating App,config,db,server files basic information

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const cors=require('cors');
+const app=express();
+const { NotFoundError }=require('./expressError');
+const morgan=require('morgan');
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('tiny'));
+
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+    return next(new NotFoundError());
+  });
+
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
+  
+    return res.status(status).json({
+      error: { message, status },
+    });
+  });
+  
+  module.exports = app;  
---
+++ backend/config.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const SECRET_KEY=process.env.SECRET_KEY || 'secret-dev';
+const PORT = +process.env.PORT || 3001;
+
+function getDatabaseUri(){
+    return (process.env.NODE_ENV === "test")
+        ? "books_test"
+        : process.env.DATABASE_URL || "books";
+}
+
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
+
+module.exports={
+    SECRET_KEY,
+    PORT,
+    BCRYPT_WORK_FACTOR,
+    getDatabaseUri,
+};
