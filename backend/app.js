927efc7f75713059e567a86c483144326b2afd88
metenar <metenar@gmail.com>
2021-06-05 16:42:57 -0700

    prepearing deployment

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,13 @@ app.use("/users", usersRoutes);
 app.use("/books",booksRoutes);
 app.use('/mybooks',MyBooksRoutes);
 app.use('/api',apiRoutes);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 /** Handle 404 errors -- this matches everything */
 
--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,10 @@ const {getDatabaseUri, API_KEY}=require('./config');
 
 const db=new Client({
     connectionString:getDatabaseUri(),
-    API_KEY:API_KEY
+    API_KEY:API_KEY,
+    ssl: {
+        rejectUnauthorized: false,
+      },
 });
 db.connect();
 module.exports=db;
